ae6832ee5d938e45bc65c6defee36b8e24348d41
Abhay-Garg-Cdz <abhayaggarwalg@gmail.com>
2023-10-17 21:05:11 +0530

    Serialization add and Start Implementation

--- BillingXpert/src/com/billingxpert/views/LoginPage.java
+++ BillingXpert/src/com/billingxpert/views/LoginPage.java
@@ -1,12 +1,30 @@
 package com.billingxpert.views;
 
+//import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.Image;
+import javax.swing.ImageIcon;
 import java.awt.EventQueue;
+import java.awt.Font;
+//import java.awt.image.*;
 
+//import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+//import javax.swing.SwingConstants;
+import javax.swing.JTextField;
 
-public class LoginPage {
+@SuppressWarnings("serial")
+public class LoginPage extends JFrame{
 
-	private JFrame frame;
+	/**
+	 * 
+	 */
+//	private JFrame frame;
+	private JTextField textField;
+	private JPasswordField passwordField;
 
 	/**
 	 * Launch the application.
@@ -16,7 +34,6 @@ public class LoginPage {
 			public void run() {
 				try {
 					LoginPage window = new LoginPage();
-					window.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -28,16 +45,60 @@ public class LoginPage {
 	 * Create the application.
 	 */
 	public LoginPage() {
-		initialize();
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(500,600);
+		setResizable(false);
+		setTitle("Login");
+		setLocationRelativeTo(null);
+		JLabel welcome=new JLabel("Login");
+		welcome.setFont(new Font("Segoe Print", Font.BOLD | Font.ITALIC, 50));
+		Container container =this.getContentPane();
+		container.setLayout(null);
+		welcome.setBounds(177,215,170,78);
+		container.add(welcome);
+		
+//		JLabel imgLabel = new JLabel("");
+//		ImageIcon image_1 = new ImageIcon(ClassLoader.getSystemResource("res/images/billingXpert.jpg"));
+//		Image image_2 = image_1.getImage().getScaledInstance(100, 50, Image.SCALE_DEFAULT);
+//		imgLabel.setIcon(new ImageIcon(image_2));
+//		imgLabel.setBounds(177, 200, 138, 74);
+//		getContentPane().add(imgLabel);
+		
+		textField = new JTextField();
+		textField.setBounds(208, 327, 236, 32);
+		getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("UserID :");
+		lblNewLabel_1.setFont(new Font("Segoe UI Variable", Font.BOLD | Font.ITALIC, 22));
+		lblNewLabel_1.setBounds(68, 321, 96, 32);
+		getContentPane().add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel(" Password :");
+		lblNewLabel_2.setFont(new Font("Segoe UI Variable", Font.BOLD | Font.ITALIC, 22));
+		lblNewLabel_2.setBounds(68, 384, 127, 25);
+		getContentPane().add(lblNewLabel_2);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(208, 386, 236, 32);
+		getContentPane().add(passwordField);
+		
+		JButton btnNewButton = new JButton("Login");
+		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 20));
+		btnNewButton.setBounds(76, 474, 145, 42);
+		getContentPane().add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Register\r\n");
+		btnNewButton_1.setFont(new Font("Tahoma", Font.BOLD, 20));
+		btnNewButton_1.setBounds(268, 474, 145, 42);
+		getContentPane().add(btnNewButton_1);
+		
+		JLabel imageLabel = new JLabel();
+		imageLabel.setBounds(144, 27, 203, 178);
+		ImageIcon image_1 = new ImageIcon(ClassLoader.getSystemResource("res/images/billingXpert.png"));
+		Image image_2 = image_1.getImage().getScaledInstance(225, 150, Image.SCALE_DEFAULT);
+		imageLabel.setIcon(new ImageIcon(image_2));
+		getContentPane().add(imageLabel);
+		setVisible(true);
 	}
-
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
-
 }
--- BillingXpert/src/com/billingxpert/views/Start.java
+++ BillingXpert/src/com/billingxpert/views/Start.java
@@ -1,27 +1,32 @@
 package com.billingxpert.views;
 
 import java.awt.EventQueue;
+import java.awt.Image;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class Start {
 
-	private JFrame frame;
+	private JFrame frameStart;
 
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
+		
 				try {
-					Start window = new Start();
-					window.frame.setVisible(true);
+					new Start();	
+					
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
+				finally {
+					new LoginPage();
+				
 			}
-		});
+		
 	}
 
 	/**
@@ -33,11 +38,32 @@ public class Start {
 
 	/**
 	 * Initialize the contents of the frame.
+	 * @throws InterruptedException 
 	 */
-	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	private void initialize()  {
+		frameStart = new JFrame();
+		frameStart.setBounds(100, 100, 412, 353);
+		frameStart.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		
+		JLabel imageLabel = new JLabel();
+		imageLabel.setBounds(144, 27, 203, 178);
+		ImageIcon image_1 = new ImageIcon(ClassLoader.getSystemResource("res/images/billingXpert.png"));
+		Image image_2 = image_1.getImage().getScaledInstance(405, 349, Image.SCALE_DEFAULT);
+		imageLabel.setIcon(new ImageIcon(image_2));
+		frameStart.getContentPane().add(imageLabel);
+		frameStart.setVisible(true);
+		
+		try {
+			Thread.sleep(3000);
+			}
+			catch(InterruptedException ie) {
+				ie.printStackTrace();
+			}
+		finally {
+			frameStart.dispose();
+		}
+//		new LoginPage();
+//		frameStart.dispose();
 	}
 
 }
